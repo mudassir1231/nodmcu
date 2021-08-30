ae8576212d0d3a903ac7e7ec6a95de1cb7631146
mudassir1231 <88880988+mudassir1231@users.noreply.github.com>
2021-08-30 17:24:27 +0530

    Add files via upload
    this project is to on and off the LED with the help of request from the mobile(claint)

---
+++ project1/project1.ino
@@ -0,0 +1,48 @@
+#include <ESP8266WiFi.h>
+
+WiFiClient client;
+WiFiServer server(80);
+
+#define led 16
+
+void setup() 
+{
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  WiFi.begin("mudassir", "mudassir");
+  while(WiFi.status() != WL_CONNECTED)
+  {
+    delay(200);
+    Serial.print("..");
+  }
+  Serial.println();
+  Serial.println("NodeMCU is connected!");
+  Serial.println(WiFi.localIP());
+ 
+  
+  server.begin();
+  pinMode(led, OUTPUT);
+
+}
+
+void loop() 
+{
+  // put your main code here, to run repeatedly:
+  client = server.available();  //Gets a client that is connected to the server and has data available for reading.    
+  if (client == 1)
+  {  
+    String request =  client.readStringUntil('\n');
+    Serial.println("string ="+request);
+     request.trim();
+    if(request == "GET /ledon HTTP/1.1")
+    {
+      digitalWrite(led, HIGH);
+      
+    }
+    if(request == "GET /ledoff HTTP/1.1")
+    {
+      digitalWrite(led, LOW);
+    }
+  }
+    
+  }
